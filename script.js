f6d61b76d8d2c2c98ca083da6a58d831b0110b45
Terry Roberts <terryjbroberts@yahoo.com>
2022-08-10 16:34:53 -0500

    inital commit for basic fetch implementation

---
+++ script.js
@@ -0,0 +1,13 @@
+const element = document.querySelector(".all-food-items");
+
+fetch("http://127.0.0.1:5000/food/get")
+  .then((response) => response.json())
+  .then((response) => {
+    response.map((item) => {
+      const newItemElement = document.createElement("div");
+      const newItemText = document.createTextNode(`${item.title}`);
+      newItemElement.appendChild(newItemText);
+
+      element.appendChild(newItemElement);
+    });
+  });
